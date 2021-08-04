f784ebe0f3999b3e8539d5a24e067ee6613e7f6e
Danil <danilkuchukov303@gmail.com>
2021-08-04 15:51:02 +0300

    bfs solution

--- main.cpp
+++ main.cpp
@@ -6,23 +6,27 @@ typedef long long ll;
 ll bfs(ll vertex, ll current_length, bool visited[], const ll cities_amount, vector< vector<ll> > matrix) {
     visited[vertex] = true;
     ll ans = 10e10;
-    for (int next_vertex; next_vertex < cities_amount; next_vertex++) {
-        if (!visited[vertex]) {
+    bool all_visited = true;
+    for (int next_vertex = 0; next_vertex < cities_amount; next_vertex++) {
+        if (!visited[next_vertex]) {
             ans = min(ans, bfs(next_vertex, current_length + matrix[vertex][next_vertex], visited, cities_amount, matrix));
+            all_visited = false;
         }
     }
     visited[vertex] = false;
-    return ans + matrix[vertex][0];
+    if (all_visited)
+        return current_length + matrix[vertex][0];
+    return ans;
 }
 
 int main() {
-    vector< vector<ll> > matrix;
-    int cities_amount;
+    ll cities_amount;
     cin >> cities_amount;
+    vector< vector<ll> > matrix(cities_amount, vector<ll> (cities_amount, 0));
 
     bool vis[cities_amount];
-    for (int x = 0; x < cities_amount; x++) {
-        for (int y = 0; y < cities_amount; y++)
+    for (ll x = 0; x < cities_amount; x++) {
+        for (ll y = 0; y < cities_amount; y++)
             cin >> matrix[x][y];
         vis[x] = false;
     }
